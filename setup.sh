2d8e3e93c5ab9ea0210cd7679bd5ed042d223c0d
Puzan4000kg <s3ovva@gmail.com>
2025-07-24 12:38:36 +0300

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+set -e
+
+# Download spaCy models
 python -m spacy download en_core_web_sm
 python -m spacy download ru_core_news_sm
-python -m nltk.downloader punkt stopwords averaged_perceptron_tagger 
+
+# Download NLTK data
+python -m nltk.downloader punkt stopwords averaged_perceptron_tagger
+
+# Verify installations
+python -c "import spacy; nlp=spacy.load('en_core_web_sm'); print('spaCy model loaded successfully')"
+python -c "import nltk; nltk.data.find('tokenizers/punkt'); print('NLTK data loaded successfully')" 
