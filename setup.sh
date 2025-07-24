0c68091d6148bf12de303cd711c78ce2306365d2
Puzan4000kg <s3ovva@gmail.com>
2025-07-24 12:29:53 +0300

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
 python -m spacy download en_core_web_sm
-python -m spacy download ru_core_news_sm 
+python -m spacy download ru_core_news_sm
+python -m nltk.downloader punkt stopwords averaged_perceptron_tagger 
